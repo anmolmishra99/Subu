5a0b6ad2637571082cb80b57529c67258e8eeff4
Anmol Mishra <49812717+AafatAyie@users.noreply.github.com>
2022-02-02 22:22:41 +0530

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -37,4 +37,20 @@ def unzip(filepath):
   zip_ref = zipfile.ZipFile(filepath)
   zip_ref.extractall()
   zip_ref.close()
+  
+##----------------------------------------WALK_THROUGH_DIR------------------------------------------------
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
 
